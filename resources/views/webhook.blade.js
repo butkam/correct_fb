c2cf91322c903fb84c771e58c26e98d9079709d1
Anton Polyakov <butkamone@gmail.com>
2016-04-18 21:44:31 +0200

    0.1
    - fb

--- resources/views/webhook.blade.js
+++
@@ -1,6 +0,0 @@
-app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '34893489') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
-})
