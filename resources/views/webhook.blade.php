7ead212361a072d612c2ecf0d7219ed6cc255e35
Anton Polyakov <butkamone@gmail.com>
2016-04-18 18:04:44 +0200

    0.1
    - fb check

--- resources/views/webhook.blade.php
+++ resources/views/webhook.blade.php
@@ -1,1 +1,8 @@
-CAAQdsQnKecgBAMNWuVs5YxsT5lGeg5xzZBIq7C7NcTD2cZChcENQfkj2urr48kRKTIe7ui6hkWXp0SoXM1pZCsFxxZAwhjXAgmkHy8j644ZBoZBdPoTVqKVBRanYkk7q0ZAfk3MAPRC5JvBFZB1v97S4ESKIjn7vhp4VwYJErWOFnafVLf6lDjfuCl6V51awcMp7mMgjaHZCojAZDZD
+<script type="text/javascript">
+  app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === '34893489') {
+      res.send(req.query['hub.challenge']);
+    }
+    res.send('Error, wrong validation token');
+  })
+</script>
