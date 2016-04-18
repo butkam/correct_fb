84383e3b1b73be4deaf0a258a63cccc6a37c8781
Anton Polyakov <butkamone@gmail.com>
2016-04-18 21:23:36 +0200

    0.1
    - fb verify

--- resources/views/webhook.blade.php
+++ resources/views/webhook.blade.php
@@ -1,6 +1,17 @@
-app.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === '34893489') {
-    res.send(req.query['hub.challenge']);
-  }
-  res.send('Error, wrong validation token');
-})
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <script type="text/javascript">
+      app.get('/webhook/', function (req, res) {
+        if (req.query['hub.verify_token'] === '34893489') {
+          res.send(req.query['hub.challenge']);
+        }
+        res.send('Error, wrong validation token');
+      })
+    </script>
+  </body>
+</html>
