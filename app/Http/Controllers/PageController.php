ab5bebb7eba46511f1c900f296f3d01132a85fe3
Anton Polyakov <butkamone@gmail.com>
2016-06-24 17:55:20 +0300

    test

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -28,24 +28,24 @@ class PageController extends Controller
     error_log(var_export($msg, 1));
     error_log(var_export($inlineQueryId, 1));
 
-    // $this->post(
-    //   '/answerInlineQuery', [
-    //     'inline_query_id' => $inlineQueryId,
-    //     'results' => [
-    //       'type'      => 'photo',
-    //       'id'        => $queryUniqId,
-    //       'photo_url' => 'https://dl.dropboxusercontent.com/u/4402725/test_mag.jpeg',
-    //       'thumb_url' => 'https://dl.dropboxusercontent.com/u/4402725/test_mag.jpeg'
-    // ]]);
+    $this->post(
+      '/answerInlineQuery', [
+        'inline_query_id' => $inlineQueryId,
+        'results' => [
+          'type'      => 'photo',
+          'id'        => $queryUniqId,
+          'photo_url' => 'https://dl.dropboxusercontent.com/u/4402725/test_mag.jpeg',
+          'thumb_url' => 'https://dl.dropboxusercontent.com/u/4402725/test_mag.jpeg'
+    ]]);
   }
 
-  public function post($method, $body)
+  public function post($method, $parameters)
   {
     $host = Config::get('services.telegram.host');
     $token = Config::get('services.telegram.token');
 
     $client = new \GuzzleHttp\Client();
-    $res = $client->request('POST', $host . $token . $method, $body);
+    $res = $client->request('POST', $host . $token . $method, json_encode($parameters));
 
     return error_log(var_export($res, 1));
   }
