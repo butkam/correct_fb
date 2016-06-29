e9b7a2c11ce8aad53db4d44acb36fa2a695f6402
Anton Polyakov <butkamone@gmail.com>
2016-06-29 22:16:17 +0200

    qq

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -51,7 +51,7 @@ class PageController extends Controller
     $client = new \GuzzleHttp\Client([ 'base_uri' => $host . $token . '/' ]);
 
     $client->request('POST', $method, $parameters);
-    error_log(var_dump($parameters->getBody()->getContents(), 1));
+    error_log(var_dump($parameters, 1));
 
     // var_dump($res->getBody()->getContents());
   }
