819fe2b94634a3e0b52597ca2c5e9494f5041a6b
Anton Polyakov <butkamone@gmail.com>
2016-06-29 22:39:10 +0200

    qq

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -43,6 +43,7 @@ class PageController extends Controller
 
   public function post($method, $parameters = null)
   {
+    error_log(var_export($method, 1));
     $host = Config::get('services.telegram.host');
     $token = Config::get('services.telegram.token');
 
